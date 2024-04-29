6015b1ccea238f7107f11481cbf4eaf210408e80
mjanders6 <mjanders6@gmail.com>
2024-04-28 21:42:18 -0700

    setting up dialog for updating server.properties file

--- get-minecraft-server.sh
+++ get-minecraft-server.sh
@@ -9,12 +9,36 @@ wget https://piston-data.mojang.com/v1/objects/79493072f65e17243fd36a699c9a96b43
 cd ~/server
 java -Xmx1024M -Xms1024M -jar server.jar nogui
 
+# File variables. The following is what they should be set to. 
+eula_value=true
+rcon_port=25575
+rcon_enabled=true
 
 # Cange the eula.txt file 
-# eula=true
+echo "Going to update the eula.txt file now. Change false to true:"
+echo ""
+sed -i ''s/eula=.*/eula=$eula_value/'' ~/server/eula.txt
 
 # Update the server.properties
 # This file can also be updated for a variety of configurations  
-# rcon.port=25575
-# rcon.password=SET-STRONG-PASSWORD
-# enable-rcon=true
+echo "Going to update the server.properties file:"
+echo ""
+
+# Set rcon.port=25575
+echo "Setting rcon port:"
+echo ""
+sed -i ''s/rcon.port=.*/rcon.port=$rcon_port/'' ~/server/server.properties
+
+# Set rcon.password=SET-STRONG-PASSWORD
+echo 'Enter the password you are going to use for rcon:'
+read -s rcon_passwd
+sed -i ''s/rcon.password=.*/rcon.password=$rcon_passwd/'' ~/server/server.properties
+
+# Set enable-rcon=true
+echo "Enabling rcon: "
+echo ""
+sed -i ''s/enable-rcon=.*/enable-rcon=$rcon_enabled/'' ~/server/server.properties
+
+echo ""
+echo "Complete"
+echo ""
