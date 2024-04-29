6390122ac87cecdc399caa2614995016cd6850fb
mjanders6 <mjanders6@gmail.com>
2024-04-28 22:23:09 -0700

    script dialog

--- minecraft-install.sh
+++ minecraft-install.sh
@@ -13,3 +13,23 @@ source get-minecraft-server.sh
 # Setup mcrcon
 source mcrcon-script.sh
 
+# Add minecraft.service file
+sudo cp minecraft.service /etc/systemd/system/minecraft.service
+
+# Restarting the daemon
+sudo systemctl daemon-reload
+sudo systemctl start minecraft
+sudo systemctl enable minecraft
+
+echo ""
+echo "Do you want to reboot the server? y/n"
+read -s ans_reboot
+
+if [''$ans_reboot'' -eq 'y']; then
+    echo "Going to reboot now"
+    sudo reboot
+else 
+    echo ""
+    echo "Ok, everything will work better after a good reboot."
+    echo ""
+fi
