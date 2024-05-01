51c7a8fc793541d910c4cbea3624d0269cd6245c
mjanders6 <mjanders6@gmail.com>
2024-04-30 22:55:43 -0700

    testing py

--- mc-installer.py
+++ mc-installer.py
@@ -1,5 +1,7 @@
 import inquirer
 from inquirer.themes import GreenPassion
+import subprocess
+
 
 # q = [
 #     inquirer.Text("name", message="Whats your name?", default="No one"),
@@ -15,7 +17,9 @@ from inquirer.themes import GreenPassion
 text = inquirer.text(message="Enter your username")
 print(text)
 
-password = inquirer.password(message='Please enter your password'),
+password = inquirer.password(message='Please enter your password')
+
+
 choice = inquirer.list_input("Public or private?",
                               choices=['public', 'private'])
 correct = inquirer.confirm("This will delete all your current labels and "
