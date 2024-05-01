9c3f3901b1961a98a3dee1df919dc6aa53d126fb
mjanders6 <mjanders6@gmail.com>
2024-05-01 12:24:58 -0700

    able to run foundation

--- mc-installer.py
+++ mc-installer.py
@@ -8,6 +8,7 @@
 import inquirer
 from inquirer.themes import GreenPassion
 import subprocess
+from subprocess import STDOUT
 import os
 import shutil
 
@@ -21,24 +22,24 @@ dest_path=r"/etc/systemd/system/minecraft.service"
 
 
 # Run the commands with no output 
-#proc = subprocess.Popen(f'sudo apt upgrade', shell=True, stdin=None, stdout=open(os.devnull,"wb"), stderr=STDOUT, executable="/bin/bash")
+proc = subprocess.Popen(f'sudo apt upgrade', shell=True, stdin=None, stdout=open(os.devnull,"wb"), stderr=STDOUT, executable="/bin/bash")
 # Run updates and upgrades
-proc = subprocess.Popen('sudo apt update && sudo apt upgrade', shell=True, stdin=None)
+#proc = subprocess.Popen('sudo apt update && sudo apt upgrade', shell=True, stdin=None)
+print("Running updates and insstalling necessary packages.")
 proc.wait()
+print("")
 
 
 # Install dependancies
 install_dependancies = subprocess.Popen(f'sudo apt install {GIT} {BUILD_Essential} {OPENJDK}', shell=True, stdin=None)
 install_dependancies.wait()
+print("")
 
 # Set password
 password = inquirer.password(message='Please enter your password for mcron: ')
 
 # Copy minecraft.service to the /etc/systemd/system location
 shutil.copyfile(src_pth, dest_path)
-
-# Change password in the service file 
-alter_file(dest_path, "strong-password", password)
 
 def alter_file(file_path, old_word, new_word):
     # Read the content of the file
@@ -52,7 +53,8 @@ def alter_file(file_path, old_word, new_word):
     with open(file_path, 'w') as file:
         file.write(modified_content)
 
-
+# Change password in the service file 
+alter_file(dest_path, "strong-password", password)
 
 
 # choice = inquirer.list_input("Public or private?",
