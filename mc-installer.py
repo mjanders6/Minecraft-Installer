6d450ae210fc7a7686d6b6b1283c31f75952158a
mjanders6 <mjanders6@gmail.com>
2024-05-01 13:24:48 -0700

    adding minecraft user functions

--- mc-installer.py
+++ mc-installer.py
@@ -17,31 +17,18 @@ GIT = 'git'
 BUILD_Essential = 'build-essential'
 OPENJDK = 'openjdk-21-jre-headless'
 
+EULA_VALUE = true
+RCON_PORT = 25575
+RCON_ENABLE = true
+
+# minecraft path
+MC_PATH = '/opt/minecraft'
+
 # Paths for service file
 src_pth = r"./minecraft.service"
 dest_path=r"/etc/systemd/system/minecraft.service"
 
-
-# Run the commands with no output 
-proc = subprocess.Popen(f'sudo apt upgrade', shell=True, stdin=None, stdout=open(os.devnull,"wb"), stderr=STDOUT, executable="/bin/bash")
-# Run updates and upgrades
-#proc = subprocess.Popen('sudo apt update && sudo apt upgrade', shell=True, stdin=None)
-print("Running updates and insstalling necessary packages.")
-proc.wait()
-print("")
-
-
-# Install dependancies
-install_dependancies = subprocess.Popen(f'sudo apt install {GIT} {BUILD_Essential} {OPENJDK}', shell=True, stdin=None)
-install_dependancies.wait()
-print("")
-
-# Set password
-password = inquirer.password(message='Please enter your password for mcron: ')
-
-# Copy minecraft.service to the /etc/systemd/system location
-shutil.copyfile(src_pth, dest_path)
-
+# Funcations 
 def alter_file(file_path, old_word, new_word):
     # Read the content of the file
     with open(file_path, 'r') as file:
@@ -54,9 +41,40 @@ def alter_file(file_path, old_word, new_word):
     with open(file_path, 'w') as file:
         file.write(modified_content)
 
+
+# Run the update/upgrade commands with no output 
+proc = subprocess.Popen(f'sudo apt update && sudo apt upgrade', shell=True, stdin=None, stdout=open(os.devnull,"wb"), stderr=STDOUT, executable="/bin/bash")
+print("Running updates and insstalling necessary packages.")
+proc.wait()
+print("")
+
+# Install dependancies
+install_dependancies = subprocess.Popen(f'sudo apt install {GIT} {BUILD_Essential} {OPENJDK}', shell=True, stdin=None)
+install_dependancies.wait()
+print("")
+
+# Set password
+password = inquirer.password(message='Please enter your password for mcron: ')
+
+# Copy minecraft.service to the /etc/systemd/system location
+shutil.copyfile(src_pth, dest_path)
+
+
 # Change password in the service file 
 alter_file(dest_path, "strong-password", password)
 
+# Create minecraft directories
+os.makedirs(os.path.expanduser('/opt/minecraft/{backups,tools,server}'), exist_ok=True)
+
+# Download Minecraft server file
+print("Getting the Minecraft file:")
+subprocess.run(["wget", "https://piston-data.mojang.com/v1/objects/79493072f65e17243fd36a699c9a96b4381feb91/server.jar", "-P", f'{MC_PATH}/server'])
+
+
+# Change directories and run Minecraft
+print("Changing directories and running Minecraft. This will error out since it may be its first run.")
+os.chdir(f'{MC_PATH}/server')
+subprocess.run(["java", "-Xmx1024M", "-Xms1024M", "-jar", "server.jar", "nogui"])
 
 # choice = inquirer.list_input("Public or private?",
                               # choices=['public', 'private'])
