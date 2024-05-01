dd0c0cdec69d5a8a413230935562c370141270ae
mjanders6 <mjanders6@gmail.com>
2024-04-30 22:51:59 -0700

    testing python

--- mc-installer.py
+++ mc-installer.py
@@ -1,12 +1,24 @@
 import inquirer
 from inquirer.themes import GreenPassion
 
-q = [
-    inquirer.Text("name", message="Whats your name?", default="No one"),
-    inquirer.List("jon", message="Does Jon Snow know?", choices=["yes", "no"], default="no"),
-    inquirer.Checkbox(
-        "kill_list", message="Who you want to kill?", choices=["Cersei", "Littlefinger", "The Mountain"]
-    ),
-]
+# q = [
+#     inquirer.Text("name", message="Whats your name?", default="No one"),
+#     inquirer.List("jon", message="Does Jon Snow know?", choices=["yes", "no"], default="no"),
+#     inquirer.Checkbox(
+#         "kill_list", message="Who you want to kill?", choices=["Cersei", "Littlefinger", "The Mountain"]
+#     ),
+# ]
 
-inquirer.prompt(q, theme=GreenPassion())
+# inquirer.prompt(q, theme=GreenPassion())
+
+
+text = inquirer.text(message="Enter your username")
+print(text)
+
+password = inquirer.password(message='Please enter your password'),
+choice = inquirer.list_input("Public or private?",
+                              choices=['public', 'private'])
+correct = inquirer.confirm("This will delete all your current labels and "
+                        "create a new ones. Continue?", default=False)
+						
+						
